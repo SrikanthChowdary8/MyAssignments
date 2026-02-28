877dd6318393a603509a8c39204d0f093173a7f9
SrikanthChowdary8 <vardhinenisrikanth@gmail.com>
2026-02-28 18:40:15 +0530

    Add files via upload
    Day 1 Assignment Submission

---
+++ Day1/Chrome.java
@@ -0,0 +1,14 @@
+package org.testleaf.Week1.Day1;
+
+public class Chrome {
+    public static void main(String[] args) {
+        Float version = 91.0f;
+        String developer = "Google";
+        Boolean isBeta = false;
+        int releaseYear = 2008;
+        char shortcutKey ='C';
+
+        System.out.println(version + " " + developer+ " " + isBeta + " " + releaseYear + " " + shortcutKey);
+    }
+
+}
---
+++ Day1/FireFox.java
@@ -0,0 +1,16 @@
+package org.testleaf.Week1.Day1;
+
+public class FireFox {
+    public static void main(String[] args) {
+        String browserName = "firefox";
+        Boolean isVisible = true;
+        char browserLogo = 'f';
+        Float browserVersion = 100.2f;
+        int releaseYear = 1998;
+
+        System.out.println(browserVersion + " " + browserName + " " + isVisible + " " + releaseYear + " " + browserLogo);
+
+        
+    }
+
+}
